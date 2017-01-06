a2d3afc09b6836914d940f69d9cd3b031c693020
William Cox <willubyu7@gmail.com>
2017-01-06 12:01:49 -0700

    finished assessment

--- script/mainService.js
+++ script/mainService.js
@@ -3,13 +3,13 @@ angular.module('assessment').service('mainService', function($http) {
   this.getApi = function() {
     return $http({
       method: 'GET',
-      url: 'http://practiceapi.devmounta.in/products'
+      url: 'https://practiceapi.devmounta.in/products'
     });
   }
   this.saveID = function(id) {
     return $http({
       method: 'GET',
-      url: 'http://practiceapi.devmounta.in/products/' + id
+      url: 'https://practiceapi.devmounta.in/products/' + id
     });
   }
   this.saveIT = function(it) {
